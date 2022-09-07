67f2747afc7bd7c63a69356c2051914bd001f574
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2022-09-07 15:20:22 -0300

    vim-plug: bootstrap
    # Ref: https://gist.github.com/2697681
    #
    # A good commit message looks like this:
    # --------------------------------------
    #
    #     Header line: explaining the commit in one line
    #
    #     Body of commit message is a few lines of text, explaining things
    #     in more detail, possibly giving some background about the issue
    #     being fixed, etc etc.
    #
    #     The body of the commit message can be several paragraphs, and
    #     please do proper word-wrap and keep columns shorter than about
    #     74 characters or so. That way "git log" will show things
    #     nicely even when it's indented.
    #
    # where that header line really should be meaningful, and really should be
    # just one line.  That header line is what is shown by tools like gitk and
    # shortlog, and should summarize the change in one readable line of text,
    # independently of the longer explanation.
    #

--- bin/plugin-install.sh
+++ bin/plugin-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -x
 
+## vim-plug: bootstrap
+curl \
+  --create-dirs \
+  -fLo ~/.vim/autoload/plug.vim \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
 # vim-plug from comandline
 vim +PlugInstall +qall
 
